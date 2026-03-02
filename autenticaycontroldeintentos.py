00b93ccd281c5c22a4b8defccc633486ebff1273
Luis Mejia <lmejiaidep@gmail.com>
2026-03-02 13:52:06 -0500

    Validacion de usuario y contraseña

--- autenticaycontroldeintentos.py
+++ autenticaycontroldeintentos.py
@@ -1,3 +1,20 @@
 #Definiendo el nombre de usuario y su contraseña
 user_name:"Riwi"
 user_password: 123
+#Bool que valida que el usuario y contraseña
+is_valid: bool = False
+#Contador de intentos
+attempts= 0
+
+while is_valid == False:
+    try:
+        name=input("Digite el nombre de usuario: ")
+        password=int(input("Digite su contraseña"))
+        if (name==user_name and password==user_password):
+            print("Iniciaste sesión correctamente")
+            is_valid=True
+        else:
+            print("Usuario y/o Contraseña inválidos")
+
+    except:
+        print("Contraseña inválida")
