4d5ecf9125820e0296499a60dd1dd90e614ce714
leonardo-200224 <ayalaperezleonardo24@gmail.com>
2026-03-05 16:07:03 -0500

    funcion validacion numerica if

--- validaciones.py
+++ validaciones.py
@@ -2,11 +2,22 @@
 def validar_entrada_numerica(entrada):
     try:
         numero = int(entrada)
+
+        if numero < 0:
+            print("ERROR: No se permiten números negativos.")
+            return False
+        
+        if numero == 0:
+            print("ERROR: El monto debe ser mayor a 0.")
+    
+
+        if numero > 1_000_000_000:
+            print("ERROR: El número es demasiado grande.")
+            return False
+
         return True
 
     except ValueError:
         print("ERROR: Debe ingresar un número entero válido.")
         return False
-
-
 
