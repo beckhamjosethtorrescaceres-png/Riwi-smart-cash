7729a386f422cd66154467d0678263d4a5dc709e
Fabberth Junior <88165854+fabberth@users.noreply.github.com>
2026-03-05 18:54:47 -0500

    Update Retirar.py

--- Retirar.py
+++ Retirar.py
@@ -1,11 +1,12 @@
 def retiro(saldo):
-    verdadero=True
-    while verdadero:
-        valor = int(input("Digite el valor a retirar: "))
-        if valor <= saldo:
-            saldo -= valor
-            print("Retiro exitoso. Saldo actual:", saldo)
-            verdadero=False
-        else:
-            verdadero=False
-            print("Saldo insuficiente")
+    valor = int(input("Digite el valor a retirar: "))
+
+    if valor <= 0:
+        print("El valor debe ser mayor que 0")
+    elif valor <= saldo:
+        saldo -= valor
+        print("Retiro exitoso. Saldo actual:", saldo)
+    else:
+        print("Saldo insuficiente")
+
+    return saldo
