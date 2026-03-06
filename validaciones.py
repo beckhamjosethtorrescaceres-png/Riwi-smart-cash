caa869913466aab65a381307ed52aabc937b14c6
leonardo-200224 <ayalaperezleonardo24@gmail.com>
2026-03-05 20:28:11 -0500

    validacion entrada numerica

--- validaciones.py
+++ validaciones.py
@@ -4,6 +4,9 @@ def validar_entrada_numerica(entrada):
         if numero <= 0:
             print("ERROR: El número debe ser mayor a 0.")
             return None
+        if numero > 1_000_000_000:
+            print("ERROR: El número es demasiado grande.")
+            return None
         return numero
     except ValueError:
         print("ERROR: Debe ingresar un número entero válido.")
